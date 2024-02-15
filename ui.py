42a72f26fede5ed45dfb76fb7edeb1062f2a5ac8
Gibran <gibranfahad101@gmail.com>
2024-02-14 22:40:31 -0600

    Main Interface added

--- ui.py
+++ ui.py
@@ -1,2 +1,31 @@
+from tkinter import *
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quiz Master")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.canvas = Canvas(width=300, height=250, highlightthickness=0, bg="white")
+        self.canvas.grid(row=0, column=0, columnspan=2)
+        self.question_text = self.canvas.create_text(150, 125, text="Some Question Text", fill=THEME_COLOR,
+                                                     font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        self.score_card = Label(text="Score: 0", bg=THEME_COLOR, fg="white", font=("Arial", 10, "bold"))
+        self.score_card.grid(row=0, column=1)
+
+        self.wrong_button_img = PhotoImage(file="images/false.png")
+        self.wrong_button = Button(image=self.wrong_button_img, highlightthickness=0)
+        self.wrong_button.grid(row=2, column=0)
+
+        self.right_button_img = PhotoImage(file="images/true.png")
+        self.right_button = Button(image=self.right_button_img, highlightthickness=0)
+        self.right_button.grid(row=2, column=1)
+
+
+
+
+        self.window.mainloop()
